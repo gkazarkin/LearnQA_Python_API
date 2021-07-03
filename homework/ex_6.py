5798d3489dbf25a38e24285badb0a927731440d1
gkazarkin <gkazarkin@sinergo.ru>
2021-07-03 21:38:06 +0700

    Ex6 update

--- homework/ex_6.py
+++ homework/ex_6.py
@@ -4,7 +4,12 @@ import requests
 response = requests.post("https://playground.learnqa.ru/api/long_redirect", allow_redirects=True)
 responses = response.history
 number_response = len(responses)
-print(number_response)
+redirects = number_response - 1
+print(redirects)
 last_response = responses[-1]
 print(last_response.url)
 
+# print(responses[0].url)
+# print(responses[1].url)
+# print(responses[2].url)
+# print(responses[3].url)
