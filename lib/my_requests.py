1eda8d25a01a3e5c01186a116a44c5dfdaba871b
Gleb Kazarkin <gkazarkin@sinergo.ru>
2021-07-19 02:02:59 +0700

    Environment + Fix errors

--- lib/my_requests.py
+++ lib/my_requests.py
@@ -1,6 +1,7 @@
 import requests
 from lib.logger import Logger
 import allure
+from environment import ENV_OBJECT
 
 class MyRequests:
     @staticmethod
@@ -23,7 +24,7 @@ class MyRequests:
     # _ в названии функции условно обозначает приватную функцию
     @staticmethod
     def _send(url: str, data: dict, headers: dict, cookies: dict, method: str):
-        url = f"https://playground.learnqa.ru/api/{url}"
+        url = f"{ENV_OBJECT.get_base_url()}{url}"
 
         if headers is None:
             headers = {}
